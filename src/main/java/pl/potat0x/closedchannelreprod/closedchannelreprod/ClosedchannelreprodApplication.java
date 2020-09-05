d97e01314e4655ef11524cc7dce8af3202bab4fc
Potat0x <Potat0x@users.noreply.github.com>
2020-09-05 10:19:51 +0200

    Init

---
+++ src/main/java/pl/potat0x/closedchannelreprod/closedchannelreprod/ClosedchannelreprodApplication.java
@@ -0,0 +1,12 @@
+package pl.potat0x.closedchannelreprod.closedchannelreprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosedchannelreprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosedchannelreprodApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/potat0x/closedchannelreprod/closedchannelreprod/ClosedchannelreprodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.potat0x.closedchannelreprod.closedchannelreprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosedchannelreprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
